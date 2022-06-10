f5734739efae862475198b632813f64f8ac15724
yorkchris-yao <yorkchris@163.com>
2022-06-10 20:20:57 +0800

    hot-fix first-commit

--- src/main/java/Demo2.java
+++ src/main/java/Demo2.java
@@ -2,5 +2,6 @@ public class Demo2 {
     public static void main(String[] args) {
         System.out.println("你好，第一次修改");
         System.out.println("你好，第二次修改");
+        System.out.println("你好，hot-fix第一次修改");
     }
 }
